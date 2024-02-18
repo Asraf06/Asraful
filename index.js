5ea238542d0eb7f85db22dd4d6593579d544b8e3
Asraf06 <159440671+Asraf06@users.noreply.github.com>
2024-02-18 21:12:23 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -39,3 +39,14 @@ function eraseWord() {
 }
 
 typeWord();
+
+VanillaTilt.init(document.querySelectorAll(".card"),{
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
+    })
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    }
